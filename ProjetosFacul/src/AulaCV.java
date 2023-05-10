5c92dd53e89d8fe89ca2139404f4ee4cae451566
Igor Rosa Ramalho <pacfa_sky@hotmail.com>
2023-05-10 15:56:39 -0400

    Atualização do programa com classe

--- ProjetosFacul/src/AulaCV.java
+++ ProjetosFacul/src/AulaCV.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 
 public class AulaCV{
-    
+    Scanner leitor = new Scanner(System.in);
     public static void main(String[] args) throws Exception{
         
         Caneta c1 = new Caneta();
-        Scanner leitor = new Scanner(System.in);
-
+ 
         c1.pedindoinfo();
 
         c1.status();
-        
-    
 
     }
 
--- ProjetosFacul/src/Caneta.java
+++ ProjetosFacul/src/Caneta.java
@@ -1,7 +1,9 @@
+import java.lang.ref.Cleaner;
 import java.util.Scanner;
 
 public class Caneta{
-    
+    Scanner leitor = new Scanner(System.in);
+
     String modelo;
     String cor;
     float ponta;
@@ -12,16 +14,17 @@ public class Caneta{
         System.out.print("Qual o modelo da caneta ->");
         this.modelo = leitor.nextLine();
         
-        System.out.println("Está tampada? ->");
-        this.tampada = leitor.nextLine();
+        System.out.print("Está tampada? ->");
+        this.tampada = leitor.nextBoolean();
         
-        System.out.println("Qual a ponta da caneta ->");
-        this.ponta = leitor.nextLine();
+        System.out.print("Qual a ponta da caneta ->");
+        this.ponta = leitor.nextFloat();
         
-        System.out.println("Qual a carga da caneta ->");
-        this.carga = leitor.nextLine();
+        System.out.print("Qual a carga da caneta ->");
+        this.carga = leitor.nextInt();
         
-        System.out.println("Qual a cor da caneta ->");
+      
+        System.out.print("Qual a cor da caneta ->");
         this.cor = leitor.nextLine();
 
     }
@@ -29,7 +32,8 @@ public class Caneta{
     
     
     void status(){
-        System.out.print( "Uma caneta -> "+ this.cor);
+        System.out.println( "------------------xxxxxxxxx----------------xxxxxxxxx----------");
+        System.out.println( "Uma caneta -> "+ this.cor);
         System.out.println("Está tampada? ->" + this.tampada);
         System.out.println("Ponta da caneta ->"+ this.ponta);
         System.out.println("Carga da caneta ->"+ this.carga);
