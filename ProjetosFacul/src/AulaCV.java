76e8a746303b00cb6129556809576a2ae735e53c
unknown <ra191807@acad.ucdb.br>
2023-05-10 22:26:04 -0400

    Atualização do programa em java

--- ProjetosFacul/src/AulaCV.java
+++ ProjetosFacul/src/AulaCV.java
@@ -9,8 +9,30 @@ public class AulaCV{
         c1.pedindoinfo();
 
         c1.status();
+        int cons = 0;
+        while(cons == 0){
+            
+            System.out.print("\t1- Rabisar com a caneta\t\t2-Tampar a caneta\n\t3-Fechar a caneta\t\t4- Sair das opções");
+            System.out.print("Escolha a opção desejada ->");
+            int op = leitor.nextInt();
+            switch(op){
+                case 1:
+                    c1.rabiscar();
+                    break;
+                case 2:
+                    c1.tampar();
+                    break;
+                case 3:
+                    c1.destamapar();
+                    break;
+                case 4:
+                    op = 1;
+                    break;
+            }
+        }
 
     }
-
 }
+
+
 
--- ProjetosFacul/src/Caneta.java
+++ ProjetosFacul/src/Caneta.java
@@ -9,10 +9,10 @@ public class Caneta{
     float ponta;
     int carga;
     boolean tampada;
+    String estado;
     
     void pedindoinfo(){
-        System.out.print("Qual o modelo da caneta ->");
-        this.modelo = leitor.nextLine();
+        
         
         System.out.print("Está tampada? ->");
         this.tampada = leitor.nextBoolean();
@@ -23,30 +23,42 @@ public class Caneta{
         System.out.print("Qual a carga da caneta ->");
         this.carga = leitor.nextInt();
         
-      
         System.out.print("Qual a cor da caneta ->");
         this.cor = leitor.nextLine();
+        
+        System.out.print("Qual o modelo da caneta ->");
+        this.modelo = leitor.nextLine();
+
+        System.out.print("A caneta é nova? ->");
+        this.estado = leitor.nextLine();
 
     }
     
-    
-    
     void status(){
         System.out.println( "------------------xxxxxxxxx----------------xxxxxxxxx----------");
-        System.out.println( "Uma caneta -> "+ this.cor);
+        System.out.println( "Cor da Caneta -> "+ this.cor);
         System.out.println("Está tampada? ->" + this.tampada);
         System.out.println("Ponta da caneta ->"+ this.ponta);
         System.out.println("Carga da caneta ->"+ this.carga);
         System.out.println("Modelo da caneta ->"+ this.modelo);
-    }
-    
-    
+        System.out.println("A caneta é nova ->"+ this.estado);
+    }  
+   
     void rabiscar(){
+        if(this.tampada== true){
+            System.out.println("A caneta está fechada");
+        }
+        else{
+            System.out.println("A caneta está rabiscando");
+            carga = carga - 1;
+        }
     }
 
     void tampar(){
+        tampada = true;
     }
     
     void destamapar(){
+        tampada = false;
     }
 }
