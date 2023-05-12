ad101f97379ed0216bf7f909b8898bba6c2dc2ec
Igor Rosa Ramalho <pacfa_sky@hotmail.com>
2023-05-11 20:10:54 -0400

    PROGRAMA FACUL 2.0

--- ProjetosFacul/Trabalho 1/src/App.java
+++ ProjetosFacul/Trabalho 1/src/App.java
@@ -1,73 +1,15 @@
-import java.util.Scanner;
+
 
 public class App {
    
     public static void main(String[] args) throws Exception {
-        int[] mesas = new int[11];
+        
         Menus menu = new Menus();
         ajuda util = new ajuda();
-        Scanner leitor = new Scanner(System.in);
-        int soma = 0;
-        int MenuAbrirmesa = 0;
-        int pw = 0;
-        
-        do{
-        menu.menuinicial();
-        int MenuInicial = leitor.nextInt();
-        
-       switch(MenuInicial){
-            
-            case 1:
-            util.limpatela();    
-            System.out.print("Mesas Disponíveis ->");   
-                for(int i=1; i<11; i++){
-                    soma += mesas[i];
-                    if(mesas[i] == 0){
-                        System.out.print(i + " - ");
-                    }  
-                }
-                if(soma<10){
-                    menu.Abrirmesa();
-                    MenuAbrirmesa = leitor.nextInt();
-                }
-                        
-                if(soma==10){
-                        util.limpatela();
-                        System.out.println("\t\t\n NÃO HÁ MESAS DISPONÍVEIS");
-                        menu.Abrirmesa();
-                        MenuAbrirmesa = leitor.nextInt();
-                    }
-                    
-                    
-               
-                switch(MenuAbrirmesa){
-                        case 1:
-                            System.out.println("---------------------------------------------------------------\n");   
-                            System.out.print("Indique o número da mesa que deseja abrir ->");
-                            int MesaEscolhida = leitor.nextInt();
-                            if (mesas[MesaEscolhida] == 1){
-                                System.out.println("\n\t\tMESA "+ MesaEscolhida +" INDISPONÍVEL");
-                                menu.Abrirmesa();
-                                MenuAbrirmesa = leitor.nextInt();
-                                
-                            }
-                            if (mesas[MesaEscolhida] == 0){
-                                mesas[MesaEscolhida] = 1;
-                                System.out.println("\n\t\tMESA "+ MesaEscolhida + " ABERTA!");
-                                continue;
-                            }
-
-                        case 2:
-                            util.limpatela();
-                            continue;
-                            
-                 }               
-                        
-        }
-    } while(pw == 0);
-
-           
-
        
+        
+       
+        menu.menuinicial();
+
     }
 }
--- ProjetosFacul/Trabalho 1/src/Menus.java
+++ ProjetosFacul/Trabalho 1/src/Menus.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Menus {
 
+   int MenuAbrirmesa;
+   Scanner leitor = new Scanner(System.in);
+   int[] mesas = new int[11];
+   ajuda util = new ajuda();
+   int soma = 0;
+   
+   
     void menuinicial(){
         System.out.println("---------------------------------------------------------------");
         System.out.println("\t\tRESTAURANTE DOIS IRMÕES");
@@ -7,6 +16,32 @@ public class Menus {
         System.out.println("\t1. Abrir mesa\n\t2. Verificar mesa\n\t3. Fechar mesa");
         System.out.println("---------------------------------------------------------------");
         System.out.print("Escolha uma opção->");
+        int MenuInicial = leitor.nextInt();
+
+        switch(MenuInicial){
+            
+            case 1:
+            util.limpatela();    
+            System.out.print("Mesas Disponíveis ->");   
+                for(int i=1; i<11; i++){
+                    soma +=mesas[i];
+                    if(mesas[i] == 0){
+                        System.out.print(i + " - ");
+                    }  
+                }
+                if(soma<10){
+                    Abrirmesa();
+                   
+                }
+                        
+                if(soma==10){
+                        util.limpatela();
+                        System.out.println("\t\t\n NÃO HÁ MESAS DISPONÍVEIS");
+                        Abrirmesa();
+                        
+                    }
+   
+        }
         
     }
 
@@ -15,8 +50,35 @@ public class Menus {
         System.out.println("\n---------------------------------------------------------------");
         System.out.println("\t1. Indicar uma mesa para ser aberta.\n\t2. Retornal ao Menu Inicial\n");
         System.out.print("Escolha uma opção->");
+        MenuAbrirmesa = leitor.nextInt();
+
+        switch(MenuAbrirmesa){
+            case 1:
+                System.out.println("---------------------------------------------------------------\n");   
+                System.out.print("Indique o número da mesa que deseja abrir ->");
+                int MesaEscolhida = leitor.nextInt();
+                if (mesas[MesaEscolhida] == 1){
+                    System.out.println("\n\t\tMESA "+ MesaEscolhida +" INDISPONÍVEL");
+                   Abrirmesa();
+                    
+                }
+                if (mesas[MesaEscolhida] == 0){
+                    mesas[MesaEscolhida] = 1;
+                    System.out.println("\n\t\tMESA "+ MesaEscolhida + " ABERTA!");
+                    menuinicial();
+                    
+                }
+
+            case 2:
+                util.limpatela();
+                menuinicial();
+                
+                
+     }               
 
     }
+
+    
 
 
 
