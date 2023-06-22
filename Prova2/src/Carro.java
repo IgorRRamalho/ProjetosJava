745f704bf18d90101e3c00fb159db37873ed65ec
Igor Rosa Ramalho <pacfa_sky@hotmail.com>
2023-06-22 19:18:18 -0400

    A

---
+++ Prova2/src/Carro.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Carro {
+
+    private String modelo, fabricante;
+    private int anoFabricação, quilometrosRodados;
+    private double consumo, valor;
+    Scanner leitor = new Scanner(System.in);
+
+
+    public Carro(){
+
+        System.out.print
+    }
+
+    https://prod.liveshare.vsengsaas.visualstudio.com/join?DEED3205BAA063C5D7BEEA7704F8149DAB74
+    
+}
