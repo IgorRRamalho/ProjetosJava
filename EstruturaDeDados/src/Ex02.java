14a1218ea111b5594363ebf388b3073994c8b115
Igor <pacfa_sky@hotmail.com>
2023-08-05 01:33:26 -0400

    .

--- EstruturaDeDados/src/Ex02.java
+++ EstruturaDeDados/src/Ex02.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
@@ -16,9 +18,9 @@ public class Ex02 {
         Scanner leitorArquivo = null;
         // Declarar uma ArrayList para receber as linhas do arquivo
         ArrayList<String> linhasArquivo = new ArrayList<String>();
-        ArrayList<String> palavra = new ArrayList<String>();
+        char[] palavra = null;
         // Declarando objeto meuArquivo da classe File
-        File meuArquivo = new File("ProjetosJava-1\\EstruturaDeDados\\src\\jogodaforca\\palavras.txt");
+        File meuArquivo = new File("C:\\GitHub\\ProjetosJava\\EstruturaDeDados\\src\\jogodaforca\\palavras.txt");
         //Fazendo a leitura
         try{
             // Instanciando leitorArquivo
@@ -38,29 +40,39 @@ public class Ex02 {
 
         NumE = alea.nextInt(10);
         // Imprimindo o conteúdo do arquivo
-        for (int i = 0; i<linhasArquivo.size();i++){
-            palavra.add(linhasArquivo.get(NumE));
-        }
+       
+            palavra = (linhasArquivo.get(0)).toCharArray();
         
-        int tamanho = palavra.size();
-        int lcertas = 0;
+        
+        
+        int tamanho = palavra.length;
+        int lcertas = 0, i;
         boolean teste = false;
         while(!teste){
-            System.out.printf("Digite uma letra ->");
-            String letra = leitor.nextLine();
-            if(palavra.contains(letra)){
-                System.out.printf("Acertou a letra %s", letra);
-                lcertas++;
+            Reader leia = new InputStreamReader(System.in);
+            System.out.printf("\nDigite uma letra ->");
+            int testealou = leitor.nextInt();
+            int letra = leia.read(palavra);
+            char letrA = (char) letra;
+            
+                for(i=0; i<palavra.length; i++){
+                    if(letrA == palavra[i]){
+                    System.out.printf("\nAcertou a letra %s", letra);
+                    lcertas++;
+                    palavra[i] = 0;
+                    }
+                }
+                if(lcertas==tamanho){
+                    System.out.printf("\nACERTOU TODAS AS LETRAS\nA PALAVRA ERA"+ linhasArquivo.get(NumE));
+                    teste=true;
+                    
+                }
+                if(letra != palavra[i]){
+                    System.out.printf("\nLetra errada");
+                }
             }
-            if(lcertas==tamanho){
-                System.out.printf("\nACERTOU TODAS AS LETRAS\nA PALAVRA ERA"+ palavra.get(NumE));
-                teste=true;
-            }
-            else{
-                System.out.printf("Letra errada");
-            }
-
-       }
 
     }
+
 }
+
